4d1c7d9aae8ac973a60da3240acb105c61c032c3
Himanshumt009 <ee24mt009@iitdh.ac.in>
2024-09-22 23:22:43 +0530

    introduce Interrupt

--- G26_lab06b/main.c
+++ G26_lab06b/main.c
@@ -18,3 +18,21 @@ int main(void) {
         //  do nothing.
     }
 }
+void init(void) {
+
+    SYSCTL_RCGCGPIO_R |= 0x020;
+    GPIO_PORTF_LOCK_R = 0x4C4F434B;
+    GPIO_PORTF_CR_R = 0x10;
+    GPIO_PORTF_DIR_R = 0x02;
+    GPIO_PORTF_DEN_R = 0x12; // digital enable for PF1 and PF4
+    GPIO_PORTF_PUR_R |= 0x01;  // enable pullup resistor for the switch
+
+
+    GPIO_PORTF_IS_R = 0x00;   // edge sensitive
+    GPIO_PORTF_IBE_R = 0x00;  // Interrupt controlled by IEV
+    GPIO_PORTF_IEV_R = 0x00;  // Falling edge trigger (button press)
+    GPIO_PORTF_ICR_R = 0x10;   // Clear any prior interrupt
+    GPIO_PORTF_IM_R = 0x10;    // Unmask interrupt for PF4
+
+    NVIC_EN0_R = 0x40000000 ;
+}
