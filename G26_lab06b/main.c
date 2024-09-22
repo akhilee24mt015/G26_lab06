34d6f904f680718fcefcb28036e8b3be5804f501
Himanshumt009 <ee24mt009@iitdh.ac.in>
2024-09-22 23:20:02 +0530

    introduce header and handler

--- G26_lab06b/main.c
+++ G26_lab06b/main.c
@@ -1,9 +1,12 @@
+#include <stdint.h>
+#include "tm4c123gh6pm.h"
 
+volatile uint32_t dutyCycle = 50;  // Initial duty cycle= 50%
+volatile uint32_t count = 160;    // Count Value Using 16MHz clock freq
+volatile uint32_t pressTime = 0;   // Time the button has been held down
+volatile uint8_t bp = 0; // button press as detection
 
-/**
- * main.c
- */
-int main(void)
-{
-	return 0;
-}
+void init(void);
+void Systick_Init(void);
+void INT_Handler(void);
+void Systick_Handler(void);
