f8e7e32a841742c1efb2503bd9ae54d75616aa6a
akhilee24mt015 <ee24mt015@iitdh.ac.in>
2024-09-22 23:29:40 +0530

    Initializing  GPIO for  LED and switches

--- g26_lab6A/main.c
+++ g26_lab6A/main.c
@@ -8,3 +8,11 @@ void init(void);               //  declaration of init function
 void Systick_Init(void);       // declaration  of Systick_Init function
 void Int_Handler(void);      // declaration of int handlar Function
 void Systick_Handler(void);   // declaration of Systick_handler function
+int main(void) {
+    init();   // Initializing  GPIO for  LED and switches
+    Systick_Init(); // Initialize SysTick timer for software PWM
+
+    while (1) {
+        // Main loop does nothing, PWM and duty cycle control is handled by interrupts
+    }
+}
